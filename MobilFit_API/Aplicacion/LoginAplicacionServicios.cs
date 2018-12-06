8203444d18cf596998250f873b2bb15f4f45c1f1
Sebastian <sebastianhenao788@gmail.com>
2018-12-06 00:15:20 -0300

    correccion de envio de contraseñas

--- MobilFit_API/Aplicacion/LoginAplicacionServicios.cs
+++ MobilFit_API/Aplicacion/LoginAplicacionServicios.cs
@@ -30,7 +30,7 @@ namespace MobilFit_API.Aplicacion
                     FROM Usuario U 
                     LEFT JOIN Usuario_Objetivo UO ON UO.id_usuario = U.id_usuario
                     LEFT JOIN Nivel_Usuario NU ON NU.id_usuario = U.id_usuario
-                    WHERE U.email = '"+email+"' and U.contraseña = '"+password+"'";
+                    WHERE U.email = '" + email + "' and U.contraseña = '" + password + "'";
 
             sqlCommand = new SqlCommand(sql, connection);
             connection.Open();
@@ -67,17 +67,17 @@ namespace MobilFit_API.Aplicacion
             string sql = string.Empty;
             sql += @"DECLARE @ULTIMO_ID INT
                     INSERT INTO Usuario (nombre, apellido, edad, sexo, email, contraseña, fecha_registro, peso, altura," +
-                                    "id_tipocuerpo)  VALUES ('" + usuario.nombre + "', '" + usuario.apellido+ "', " +
-                                    + usuario.edad + ", " + usuario.sexo + ", '" + usuario.email + "', '" + usuario.contraseña + "', '" + fecha + "'," +
+                                    "id_tipocuerpo)  VALUES ('" + usuario.nombre + "', '" + usuario.apellido + "', " +
+                                    +usuario.edad + ", " + usuario.sexo + ", '" + usuario.email + "', '" + usuario.contraseña + "', '" + fecha + "'," +
                                     "" + usuario.peso + ", " + usuario.altura + ", " + usuario.id_tipoCuerpo + ")" +
                                     "(SELECT @ULTIMO_ID = scope_identity())" +//Rescata el utlimo ID_usuario insertado para insertarlo en las tablas de relacionadas al usuario
                                     "INSERT INTO Usuario_Objetivo (id_objetivo, id_usuario) VALUES (" + usuario.id_objetivo + ", @ULTIMO_ID) " +
-                                    "INSERT INTO Nivel_Usuario (id_usuario, id_nivel) VALUES (@ULTIMO_ID, "+usuario.id_nivel+") " +
-                                    "SELECT U.id_usuario, U.nombre, U.apellido, U.edad, U.sexo, U.email, U.contraseña, U.fecha_registro, U.peso, U.altura,"+
-                                    "U.id_tipocuerpo, NU.id_nivel, UO.id_objetivo"+
-                                    " FROM Usuario U"+
-                                    " LEFT JOIN Usuario_Objetivo UO ON UO.id_usuario = U.id_usuario"+
-                                    " LEFT JOIN Nivel_Usuario NU ON NU.id_usuario = U.id_usuario"+
+                                    "INSERT INTO Nivel_Usuario (id_usuario, id_nivel) VALUES (@ULTIMO_ID, " + usuario.id_nivel + ") " +
+                                    "SELECT U.id_usuario, U.nombre, U.apellido, U.edad, U.sexo, U.email, U.contraseña, U.fecha_registro, U.peso, U.altura," +
+                                    "U.id_tipocuerpo, NU.id_nivel, UO.id_objetivo" +
+                                    " FROM Usuario U" +
+                                    " LEFT JOIN Usuario_Objetivo UO ON UO.id_usuario = U.id_usuario" +
+                                    " LEFT JOIN Nivel_Usuario NU ON NU.id_usuario = U.id_usuario" +
                                     " WHERE U.id_usuario = @ULTIMO_ID";
 
             sqlCommand = new SqlCommand(sql, connection);
@@ -132,13 +132,13 @@ namespace MobilFit_API.Aplicacion
             Usuario objUsuario;
 
             string sql = string.Empty;
-            sql += @"UPDATE Usuario SET nombre = '"+usuario.nombre+"', apellido = '"+usuario.apellido+"', edad = "+usuario.edad+", sexo = "+usuario.sexo+", email = '"+usuario.email+"', "+
-                    " contraseña = '"+usuario.contraseña+"', peso = "+usuario.peso+", altura = "+usuario.altura+", id_tipocuerpo = "+usuario.id_tipoCuerpo+" " + 
-                    " WHERE id_usuario = "+id+" "+ 
-                    " UPDATE Usuario_Objetivo SET id_objetivo = "+usuario.id_objetivo+ " WHERE id_usuario = " + id + " " +
+            sql += @"UPDATE Usuario SET nombre = '" + usuario.nombre + "', apellido = '" + usuario.apellido + "', edad = " + usuario.edad + ", sexo = " + usuario.sexo + ", email = '" + usuario.email + "', " +
+                    " contraseña = '" + usuario.contraseña + "', peso = " + usuario.peso + ", altura = " + usuario.altura + ", id_tipocuerpo = " + usuario.id_tipoCuerpo + " " +
+                    " WHERE id_usuario = " + id + " " +
+                    " UPDATE Usuario_Objetivo SET id_objetivo = " + usuario.id_objetivo + " WHERE id_usuario = " + id + " " +
                     " UPDATE Nivel_Usuario SET id_nivel = " + usuario.id_nivel + " WHERE id_usuario = " + id + " " +
-                    " SELECT U.id_usuario, U.nombre, U.apellido, U.edad, U.sexo, U.email, U.contraseña, U.fecha_registro, U.peso, U.altura,U.id_tipocuerpo, NU.id_nivel, UO.id_objetivo  "+
-                    " FROM Usuario U "+ 
+                    " SELECT U.id_usuario, U.nombre, U.apellido, U.edad, U.sexo, U.email, U.contraseña, U.fecha_registro, U.peso, U.altura,U.id_tipocuerpo, NU.id_nivel, UO.id_objetivo  " +
+                    " FROM Usuario U " +
                     " LEFT JOIN Usuario_Objetivo UO ON UO.id_usuario = U.id_usuario " +
                     " LEFT JOIN Nivel_Usuario NU ON NU.id_usuario = U.id_usuario WHERE U.id_usuario = " + id + " ";
 
@@ -163,17 +163,38 @@ namespace MobilFit_API.Aplicacion
                 objUsuario.id_objetivo = int.Parse(reader["id_objetivo"].ToString());
             }
 
-            
-return objUsuario;
+
+            return objUsuario;
         }
 
         public int SendEmail(string email)
         {
+            string nuevaContraseña = string.Empty;
+            nuevaContraseña = "ajust95H";
+            SqlConnection connection = new SqlConnection(this.connection);
+            SqlCommand sqlCommand;
+            SqlDataReader reader;
+            string sql = string.Empty;
+            int actualizo = 0;
+
+            sql += @"update Usuario set contraseña = '"+nuevaContraseña+"' where email = '"+email+"'";
+
+            sqlCommand = new SqlCommand(sql, connection);
+            connection.Open();
+            reader = sqlCommand.ExecuteReader();
+            actualizo = reader.RecordsAffected;
+            connection.Close();
+
+            if (actualizo == 0)
+            {
+                return 0;
+            }
+
             MailMessage emailSended = new MailMessage();
             emailSended.To.Add(new MailAddress(email));
             emailSended.From = new MailAddress("MobilFit@MobilFit.com");
             emailSended.Subject = "Recuperación de constraseña ( " + DateTime.Now.ToString("dd / MMM / yyy hh:mm:ss") + " ) ";
-            emailSended.Body = "Su nueva contraseña MobilFit es: " + "<strong>ajusdnH96</strong>" + "\n" + "Por favor cambie la contraseña despues de iniciar sesión.";
+            emailSended.Body = "Su nueva contraseña MobilFit es: " + "<strong>" + nuevaContraseña + "</strong>" + "\n" + "Por favor cambie la contraseña despues de iniciar sesión.";
             emailSended.IsBodyHtml = true;
             emailSended.Priority = MailPriority.Normal;
 
